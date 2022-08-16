e8ef6db95b1a1cec45d2e4e8dabcd4504b46e2f9
hzhang3 <hzhang3@xiaohongshu.com>
2022-08-16 14:37:31 +0800

    feat: 路径总和

---
+++ easy/112. 二叉树路径总和.js
@@ -0,0 +1,65 @@
+/**
+ * 字节
+给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。如果存在，返回 true ；否则，返回 false 。
+
+叶子节点 是指没有子节点的节点。
+
+ 
+
+示例 1：
+
+
+输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+输出：true
+解释：等于目标和的根节点到叶节点路径如上图所示。
+示例 2：
+
+
+输入：root = [1,2,3], targetSum = 5
+输出：false
+解释：树中存在两条根节点到叶子节点的路径：
+(1 --> 2): 和为 3
+(1 --> 3): 和为 4
+不存在 sum = 5 的根节点到叶子节点的路径。
+示例 3：
+
+输入：root = [], targetSum = 0
+输出：false
+解释：由于树是空的，所以不存在根节点到叶子节点的路径。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+ var hasPathSum = function(root, targetSum) {
+  if (root === null) return false
+
+  let tag = false;
+
+  function dfs(node, s) {
+      if (node.left === null && node.right === null) {
+          if ((node.val + s) === targetSum) {
+              tag = true
+          }
+      }
+      if (node.left !== null) dfs(node.left, s + node.val);
+      if (node.right !== null) dfs(node.right, s + node.val);
+  }
+
+  dfs(root, 0)
+  return tag;
+};
